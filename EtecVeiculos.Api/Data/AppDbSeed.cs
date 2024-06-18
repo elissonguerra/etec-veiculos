8c734f010cbb6d7d5c0464576f4cb4a5b16fba6b
elissonguerra <elisson.guerra@gmail.com>
2024-06-18 20:35:30 -0300

    Adicionado Models marca e modelo, e DTOs marca de modelo

--- EtecVeiculos.Api/Data/AppDbSeed.cs
+++ EtecVeiculos.Api/Data/AppDbSeed.cs
@@ -1,13 +1,10 @@
 using EtecVeiculos.Api.Models;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 namespace EtecVeiculos.Api.Data;
 public class AppDbSeed
 {
-    public AppDbSeed()
-    {
-    }
-
     public AppDbSeed(ModelBuilder modelBuilder)
     {
         #region TipoVeiculo
@@ -17,15 +14,68 @@ public class AppDbSeed
                 Name = "Moto"
             },
             new(){
-                Id = 1,
-                Name = "Moto"
+                Id = 2,
+                Name = "Carro"
             },
             new(){
-                Id = 1,
-                Name = "Moto"
+                Id = 3,
+                Name = "Caminhão"
             }
         ];
         modelBuilder.Entity<TipoVeiculo>().HasData(tipoVeiculos);
+        #endregion
+
+        #region Marcas
+        List<Marca> marca = [
+            new(){
+                Id = 1,
+                Nome = "Fiat"
+            },
+            new(){
+                Id = 2,
+                Nome = "Chevrolet"
+            },
+            new(){
+                Id = 3,
+                Nome = "Ford"
+            }
+        ];
+        #endregion
+
+        #region Modelo
+        List<Modelo> modelos = new() {
+            new() {
+                Id = 1,
+                Nome = "Uno",
+                MarcaId = 1 
+            },
+            new() {
+                Id = 2,
+                Nome = "Pálio",
+                MarcaId = 1 
+            },
+            new() {
+                Id = 3,
+                Nome = "Celta",
+                MarcaId = 2 
+            },
+            new() {
+                Id = 4,
+                Nome = "Strada",
+                MarcaId = 2 
+            },
+            new() {
+                Id = 5,
+                Nome = "Fiesta",
+                MarcaId = 3 
+            },
+            new() {
+                Id = 6,
+                Nome = "Ká",
+                MarcaId = 3 
+            }
+        };
+        modelBuilder.Entity<Modelo>().HasData(modelos);
         #endregion
     }
 }
